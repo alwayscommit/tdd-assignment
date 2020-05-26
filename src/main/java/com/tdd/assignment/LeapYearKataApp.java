09b92d6f058f3c0b93e01746c5ecc1e36be73a5d
it-by-choice <ranglaniaakash@gmail.com>
2020-05-26 14:58:39 +0530

    leap year kata

---
+++ src/main/java/com/tdd/assignment/LeapYearKataApp.java
@@ -0,0 +1,41 @@
+package com.tdd.assignment;
+
+public class LeapYearKataApp {
+
+	public boolean isLeapYear(int year) {
+
+		boolean isLeapYear = isDivisibleBy400(year);
+
+		if (!isLeapYear) {
+			isLeapYear = divisibleBy4ButNot100(year);
+		}
+
+		return isLeapYear;
+	}
+
+	private boolean divisibleBy4ButNot100(int year) {
+		if (disibleBy4(year) && notDivisibleBy100(year)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean notDivisibleBy100(int year) {
+		return year % 100 != 0;
+	}
+
+	private boolean isDivisibleBy400(int year) {
+
+		if (year < 400) {
+			return false;
+		} else {
+			return (year % 400) == 0;
+		}
+	}
+	
+	private boolean disibleBy4(int year) {
+		return year % 4 == 0;
+	}
+
+}
---
+++ src/test/java/com/tdd/assignment/LeapYearKataTest.java
@@ -0,0 +1,80 @@
+package com.tdd.assignment;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LeapYearKataTest {
+	
+	private LeapYearKataApp leapYearTester; 
+	
+	@Before
+	public void setup() {
+		leapYearTester = new LeapYearKataApp();
+	}
+	
+	@Test
+	public void testNothingYear() {
+		boolean isLeapYear = leapYearTester.isLeapYear(1234);
+		assertEquals(false, isLeapYear);
+	}
+	
+	@Test
+	public void testDivideBy400Year() {
+		boolean isNotLeapYear = leapYearTester.isLeapYear(1234);
+		assertEquals(false, isNotLeapYear);
+		
+		boolean isLeapYear = leapYearTester.isLeapYear(400);
+		assertEquals("Is leap year :: ", true, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(800);
+		assertEquals("is 800 leap year :: ", true, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(0);
+		assertEquals("0/400 case :: ", false, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(399);
+		assertEquals("0/400 case :: ", false, isLeapYear);
+	}
+	
+	@Test
+	public void testDivisibleBy100And400Year() {
+		boolean isLeapYear = leapYearTester.isLeapYear(1800);
+		assertEquals(false, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(1900);
+		assertEquals(false, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(2100);
+		assertEquals(false, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(1700);
+		assertEquals(false, isLeapYear);
+	}
+	
+	@Test
+	public void divisibleBy4ButNot100() {
+		boolean isLeapYear = leapYearTester.isLeapYear(2008);
+		assertEquals(true, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(2012);
+		assertEquals(true, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(2016);
+		assertEquals(true, isLeapYear);
+	}
+	
+	@Test
+	public void notDivisibleBy4() {
+		boolean isLeapYear = leapYearTester.isLeapYear(2017);
+		assertEquals(false, isLeapYear);
+		
+		isLeapYear = leapYearTester.isLeapYear(2018);
+		assertEquals(false, isLeapYear);
+
+		isLeapYear = leapYearTester.isLeapYear(2019);
+		assertEquals(false, isLeapYear);
+		
+	}
+}
